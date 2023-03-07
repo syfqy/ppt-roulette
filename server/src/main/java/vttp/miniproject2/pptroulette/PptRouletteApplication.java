c119d3696f4871744913d786ee588063d0c18ed0
syfqy <syafiq.ysf@gmail.com>
2023-03-08 00:30:52 +0800

    build: add ng-material

---
+++ server/src/main/java/vttp/miniproject2/pptroulette/PptRouletteApplication.java
@@ -0,0 +1,13 @@
+package vttp.miniproject2.pptroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PptRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PptRouletteApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/vttp/miniproject2/pptroulette/PptRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.miniproject2.pptroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PptRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
